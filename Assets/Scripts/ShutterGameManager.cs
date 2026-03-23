282a6b782590865a10f776506743120b2c6d26e3
Riwu <89302213+Riwu1219@users.noreply.github.com>
2026-03-23 23:02:32 +0800

    up down

--- Assets/Scripts/ShutterGameManager.cs
+++ Assets/Scripts/ShutterGameManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ShutterGameManager : MonoBehaviour
 {
@@ -8,9 +9,12 @@ public class ShutterGameManager : MonoBehaviour
     [SerializeField]
     GameObject shutter;
     Rigidbody shutterRb;
+    [SerializeField] private float lerpSpeed = 10f;
+    [SerializeField] private float tiltAngle = 15f;
     public float hp = 100f;
     public float damageMultiplier = 1f;
     public CameraEffect cameraEffect;
+    
 
     [SerializeField]
     RectTransform UI_Grid;
@@ -34,6 +38,11 @@ public class ShutterGameManager : MonoBehaviour
         shutterRb = shutter.GetComponent<Rigidbody>();
     }
 
+    private void OnLevelWasLoaded(int level)
+    {
+        //TODO : Story
+    }
+
     private void Update()
     {
         if (isDriving) 
@@ -41,6 +50,51 @@ public class ShutterGameManager : MonoBehaviour
             
             horizontalInput = Input.GetAxis("Horizontal");
             verticalInput = Input.GetAxis("Vertical");
+
+            if (Mathf.Abs(horizontalInput) > 0.01f)
+            {
+                float z = (horizontalInput > 0) ? -tiltAngle : tiltAngle; // right = -, left = +
+                Quaternion target = Quaternion.Euler(0f, 0f, z);
+
+                shutter.transform.localRotation = Quaternion.Lerp(
+                    shutter.transform.localRotation,
+                    target,
+                    Time.deltaTime * lerpSpeed
+                );
+            }
+            else
+            {
+                // return to neutral
+                Quaternion target = Quaternion.Euler(0f, 0f, 0f);
+                shutter.transform.localRotation = Quaternion.Lerp(
+                    shutter.transform.localRotation,
+                    target,
+                    Time.deltaTime * lerpSpeed
+                );
+            }
+            if (Mathf.Abs(verticalInput) > 0.01f)
+            {
+                // up = -20, down = +20 (flip signs if you want the opposite)
+                float x = (verticalInput > 0) ? -tiltAngle : tiltAngle;
+
+                Quaternion target = Quaternion.Euler(x, 0f, 0f);
+
+                transform.localRotation = Quaternion.Lerp(
+                    transform.localRotation,
+                    target,
+                    Time.deltaTime * lerpSpeed
+                );
+            }
+            else
+            {
+                // return to neutral
+                Quaternion target = Quaternion.Euler(0f, 0f, 0f);
+                transform.localRotation = Quaternion.Lerp(
+                    transform.localRotation,
+                    target,
+                    Time.deltaTime * lerpSpeed
+                );
+            }
 
             if (shutter.transform.position.x > spaceScale && horizontalInput > 0)
             {
@@ -60,11 +114,16 @@ public class ShutterGameManager : MonoBehaviour
         
     }
 
+    private void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
 
     public void OnMeteorHit(GameObject meteor)
     {
         TakeDamage(meteor.transform.localScale.x * damageMultiplier);
         cameraEffect.TriggerShake(cameraEffect.shakeDuration, cameraEffect.shakeMagnitude * (meteor.transform.localScale.x), cameraEffect.dampingSpeed);
+        //Play hit sound effect here
         Destroy(meteor);
         // TODO: Lose condition, Back to meteor dodge start.
     }
@@ -75,9 +134,10 @@ public class ShutterGameManager : MonoBehaviour
         if (hp <= 0)
         {
             hp = 0;
-            ShutterGameManager.instance.meteorGenerator.isMeteorGenerate = false;
+            meteorGenerator.isMeteorGenerate = false;
             Debug.Log("Shutter destroyed!");
             //TODO: Lose condition, Back to meteor dodge start.
+            Invoke("RestartLevel", 3f);
         }
     }
 
